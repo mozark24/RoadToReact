a9194a4eed542c5c0d0c72be81f3e05589518041
mozark24 <mozark24@gmail.com>
2018-05-03 13:08:07 -0400

    Update commit

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const DEFAULT_QUERY = 'redux';
 
 const list = [
   {
@@ -50,27 +51,51 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search                     // Splitting up component properties. Class name
-          value={searchTerm}        // Controlled components pg60
-          onChange={this.onSearchChange}
-        />
-        <Table 
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+      <div className="page">
+        <div className="interactions">
+          <Search                     // Splitting up component properties. Class name
+            value={searchTerm}        // Controlled components pg60
+            onChange={this.onSearchChange}
+          >
+            Search                    
+          </Search>   
+        </div>                
+          <Table 
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
       </div>
     );
   }
 }
 
+const Button = ({onClick, className = '', children}) =>    // Setting default value for object, prevents undefined values when no className is specified
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
+
+// Functional Stateless component.  Doesn't require state management with "this"          
+const Search = ({ value, onChange, children }) =>            
+  <form>
+      {children} <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+
+/* Class component - not necessary with no state info required  Refactored out
 class Search extends Component {
   render() {
-    const { value, onChange } = this.props;     // Props have all values passed to the componenets in the App comp
+    const { value, onChange, children } = this.props;     // Props have all values passed to the componenets in the App comp. Children props are composable elements (pg68)
     return (
       <form>
-        <input
+        {children} <input
           type="text"
           value={value}
           onChange={onChange}
@@ -78,34 +103,32 @@ class Search extends Component {
       </form>
     );
   }
-}
+ }
+*/
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => 
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span> {item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <button
-              onClick={() => onDismiss(item.objectID)}
-              type="button"
-            >
-              Dismiss
-            </button>
-          </span>
-        </div>
-        )}
-      </div>
-    );
-  }
-}
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item => 
+    <div key={item.objectID} className="table-row">
+      <span style={{ width: '40%' }}>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span style={{ width: '30%' }}> 
+        {item.author}</span>
+      <span style={{ width: '10%' }}>
+        {item.num_comments}</span>
+      <span style={{ width: '10%' }}>
+        {item.points}</span>
+      <span style={{ width: '10%' }}>
+        <Button 
+          onClick={() => onDismiss(item.objectID)}
+          classname="button-inline"
+        >
+          Dismiss
+        </Button>
+      </span>
+    </div>
+    )}
+  </div>
 
 export default App;
