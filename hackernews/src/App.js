87d201557cb7dd6679f4ed576675a63010be5b3a
mozark24 <mozark24@gmail.com>
2018-05-03 14:18:56 -0400

    Update

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -3,24 +3,11 @@ import './App.css';
 
 const DEFAULT_QUERY = 'redux';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    poitns: 5,
-    objectID: 1,
-  },
-];
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = '${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}';
 
 const isSearched = searchTerm => item => 
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -30,12 +17,26 @@ class App extends Component {
     super(props)
 
     this.state = {
-      list: list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch('${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}')
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onSearchChange(event) {
@@ -44,12 +45,17 @@ class App extends Component {
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({ 
+      result: {...this.state.result, hits: updatedHits} 
+    });
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    
+    if (!result) { return null; }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -60,11 +66,14 @@ class App extends Component {
             Search                    
           </Search>   
         </div>                
-          <Table 
-            list={list}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
-          />
+          { result 
+            ? <Table 
+              list={result.hits}
+              pattern={searchTerm}
+              onDismiss={this.onDismiss}
+            />
+            : null
+          }
       </div>
     );
   }
@@ -88,23 +97,6 @@ const Search = ({ value, onChange, children }) =>
         onChange={onChange}
       />
     </form>
-
-/* Class component - not necessary with no state info required  Refactored out
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;     // Props have all values passed to the componenets in the App comp. Children props are composable elements (pg68)
-    return (
-      <form>
-        {children} <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
- }
-*/
 
 const Table = ({ list, pattern, onDismiss }) =>
   <div className="table">
